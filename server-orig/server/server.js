721fae774b85f361def0930fe88e81fb25d17bac
Andrew Ilijic <ailijic@gmail.com>
2016-12-20 19:50:26 -0500

    Add code coverage

---
+++ server-orig/server/server.js
@@ -0,0 +1,17 @@
+// server/server.js
+
+start()
+function start () {
+  serverStart()
+  function serverStart() {
+    const express = require('express')
+    const app = express()
+
+    app.use(express.static('public'))
+
+    app.listen(process.env.PORT || 8080)
+
+    exports.app = app
+  }
+}
+
