7707998864882bfd24120288854f1210571f71f2
Andrew Ilijic <ailijic@gmail.com>
2016-12-28 11:30:07 -0500

    Move server to the top of the domain

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ function start () {
     const app = express()
 
     app.use(express.static('public'))
-    console.log(__dirname)
-    app.use('/fullcalendar', express.static(__dirname + '/node_modules/fullcalendar/dist/'));
+    app.get('/login', (req, res) => {
+      res.sendFile(`${__dirname}/public/login.html`)
+      // res.send(`${__dirname}`)
+    })
 
     app.listen(process.env.PORT || 8080)
 
